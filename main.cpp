6eee154cee037a50dd07d687963cadd124fc0100
PCristian00 <cristianp9300@gmail.com>
2022-02-11 10:59:18 +0100

    Commenti e codice sintetizzato

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <cstring>
+
 using namespace std;
 #define NUMERO_PILOTI 20
 #define NOME_MAX 15
@@ -15,99 +16,99 @@ void leggiFile(const char nomefile[]) {
     fstream file;
     file.open(nomefile, ios::in);
     if (!file) {
-        cout <<nomefile<<" NON TROVATO!"<<endl;
+        cout << nomefile << " NON TROVATO!" << endl;
         exit(1);   // call system to stop
     }
-    for(int i=0;i<NUMERO_PILOTI;i++){
-        file>>tempi[i].pilota;
-        file>>tempi[i].tempo;
-    }
+    for (int i = 0; i < NUMERO_PILOTI; i++) file >> tempi[i].pilota >> tempi[i].tempo;
     file.close();
 }
 
 /* Ordina il vettore “giri” di lunghezza “n” in maniera crescente secondo il
 campo “giro.tempo”. */
 void ordinaTempi(giro giri[], int n) {
-giro tmp;
-for(int i=0;i<n;i++)
-    for(int j=i;j<n;j++)
-        if(strcmp(giri[i].tempo,giri[j].tempo)>0){
-            tmp=giri[i];
-            giri[i]=giri[j];
-            giri[j]=tmp;
-        }
+    giro tmp;
+    for (int i = 0; i < n; i++)
+        for (int j = i; j < n; j++)
+            if (strcmp(giri[i].tempo, giri[j].tempo) > 0) {
+                tmp = giri[i];
+                giri[i] = giri[j];
+                giri[j] = tmp;
+            }
 }
 
 /* Stampa il vettore “giri” di lunghezza “n” come mostrato nell'esempio di
 esecuzione */
 void stampaGriglia(giro giri[], int n) {
-for(int i=0;i<n;i++)
-    cout<<(i+1)<<". "<<giri[i].pilota<<" "<<giri[i].tempo<<endl;
+    for (int i = 0; i < n; i++)
+        cout << (i + 1) << ". " << giri[i].pilota << " " << giri[i].tempo << endl;
 }
 
 /* Se nel vettore “giri” esiste un elemento per cui il campo “giro.pilota” è
 uguale a “nomePilota”, lo stampa a schermo, indicandone posizione e tempo.
 Altrimenti avvisa l'utente che “nomePilota” non è presente nel vettore. */
 void stampaPilota(giro giri[], int n, char nomePilota[]) {
-    //posizione del pilota trovato
-   //cout<<nomePilota<<endl;
+    //Nome completamente in minuscolo
     strlwr(nomePilota);
-    //cout<<nomePilota<<endl;
-    nomePilota[0]=toupper(nomePilota[0]);
-    //cout<<nomePilota<<endl;
-    int pf=-1;
-for(int i=0;i<n;i++)
-    if(strcmp(giri[i].pilota,nomePilota)==0) pf=i;
+    //Prima lettera maiuscola
+    nomePilota[0] = toupper(nomePilota[0]);
 
-    if(pf!=-1)
-    cout<<giri[pf].pilota<<" partira' in posizione "<<pf+1<<" con il tempo di "<<giri[pf].tempo<<endl;
-    else cout<<"Nessun pilota in gara si chiama "<<nomePilota<<endl;
+    //posizione del pilota trovato
+    int pf = -1;
+    for (int i = 0; i < n; i++)
+        if (strcmp(giri[i].pilota, nomePilota) == 0) pf = i;
+
+    if (pf != -1)
+        cout << giri[pf].pilota << " partira' in posizione " << pf + 1 << " con il tempo di " << giri[pf].tempo << endl;
+    else cout << "Nessun pilota in gara si chiama " << nomePilota << endl;
 
 }
 
 // Programma principale: mostra il menù e chiama le altre funzioni.
 int main() {
     //TODO mettere solo "tempi.txt" pare non funzionare, modificare questo indirizzo con il path assoluto sul tuo terminale
-    char filename[]="C:\\Users\\Thinkpad User\\CLionProjects\\tempi_f1\\tempi.txt";
+    char filename[] = "C:\\Users\\Thinkpad User\\CLionProjects\\tempi_f1\\tempi.txt";
 
     leggiFile(filename);
-    ordinaTempi(tempi,NUMERO_PILOTI);
+    ordinaTempi(tempi, NUMERO_PILOTI);
 
-char scelta;
-char pilota[NOME_MAX];
-bool run= true;
+    char scelta;
+    char pilota[NOME_MAX];
+    bool run = true;
 
-do {
+    do {
 
-    cout<<endl;
-    cout<<"MENU"<<endl;
-    cout<<"A) STAMPA GRIGLIA PARTENZA"<<endl;
-    cout<<"B) STAMPA PILOTA"<<endl;
-    cout<<"C) ESCI"<<endl;
-    cout<<endl;
+        cout << endl;
+        cout << "MENU" << endl;
+        cout << "A) STAMPA GRIGLIA PARTENZA" << endl;
+        cout << "B) STAMPA PILOTA" << endl;
+        cout << "C) ESCI" << endl;
+        cout << endl;
 
-    cin>>scelta;
+        cin >> scelta;
 
-    switch (scelta) {
-        case 'a':case 'A':
-            stampaGriglia(tempi, NUMERO_PILOTI);
-            break;
+        switch (scelta) {
+            case 'a':
+            case 'A':
+                stampaGriglia(tempi, NUMERO_PILOTI);
+                break;
 
-        case 'b':case 'B':
-            cout<<"INSERIRE NOME PILOTA: "<<endl;
-            cin >> pilota;
-            stampaPilota(tempi, NUMERO_PILOTI, pilota);
-            break;
+            case 'b':
+            case 'B':
+                cout << "INSERIRE NOME PILOTA: " << endl;
+                cin >> pilota;
+                stampaPilota(tempi, NUMERO_PILOTI, pilota);
+                break;
 
-        case 'c': case 'C':
-            cout << "CHIUSURA APPLICAZIONE"<<endl;
-            run=false;
-            break;
+            case 'c':
+            case 'C':
+                cout << "CHIUSURA APPLICAZIONE" << endl;
+                run = false;
+                break;
 
-        default:
-            cout<<"SCELTA INCOMPATIBILE"<<endl;
-            break;
-    }
-} while (run);
+            default:
+                cout << "SCELTA INCOMPATIBILE" << endl;
+                break;
+        }
+    } while (run);
 
 }
